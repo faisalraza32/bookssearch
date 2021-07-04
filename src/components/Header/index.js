20a6007c748daeb08803b531b2e6ca9c33aebb08
Muhammad Raza <muhammad.raza@intelex.com>
2021-07-03 21:00:26 -0400

    Add search field

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import MenuIcon from "@material-ui/icons/Menu";
+import SearchField from "../SearchField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,6 +36,7 @@ const Header = ({ title }) => {
           >
             {title}
           </Typography>
+          <SearchField />
         </Toolbar>
       </AppBar>
     </div>
